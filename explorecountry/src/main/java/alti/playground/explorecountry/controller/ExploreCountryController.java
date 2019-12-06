f67c6a98cb6992db73bfb3bf4537cc1aa4f90d89
krishnakc239 <krishnakc2339@gmail.com>
2019-12-05 21:57:45 -0600

    sring with angualr google map project added

---
+++ explorecountry/src/main/java/alti/playground/explorecountry/controller/ExploreCountryController.java
@@ -0,0 +1,38 @@
+package alti.playground.explorecountry.controller;
+
+import alti.playground.explorecountry.domain.Country;
+import alti.playground.explorecountry.service.ExploreCountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:4200"}, maxAge = 6000)
+public class ExploreCountryController {
+
+    @Autowired
+    private ExploreCountryService exploreCountryService;
+
+    @GetMapping(value = "/api/countries/{iso2Code}", produces = "application/json")
+    public List<Country> findSimilarCountries(@PathVariable String iso2Code,
+                                               @RequestParam boolean matchRegion,
+                                               @RequestParam boolean matchIncomeLevel,
+                                               @RequestParam boolean matchLendingType){
+        System.out.println("passed parameter.......");
+        System.out.println(iso2Code);
+        System.out.println(matchRegion);
+        System.out.println(matchIncomeLevel);
+        System.out.println(matchLendingType);
+        return exploreCountryService.findSimilarCountries(iso2Code, matchRegion, matchIncomeLevel, matchLendingType);
+    }
+
+    @GetMapping(value = "/api/getcountry/{iso2Code}", produces = "application/json")
+    public Country getCountry(@PathVariable String iso2Code)
+    {
+        return exploreCountryService.getCountry(iso2Code);
+    }
+
+
+
+}
